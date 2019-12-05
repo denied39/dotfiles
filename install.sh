d28bd0fd4123472a46e25df7aa10a5d255be4391
denied39 <denied39@gmail.com>
2019-12-05 12:55:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Symlink .bashrc
 ln -sfv "$DOTFILES_DIR/.bashrc" ~
 ln -sfv "$DOTFILES_DIR/.profile" ~
-ln -sfv "$DOTFILES_DIR/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/.vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/.dircolors" ~
 ln -sfv "$DOTFILES_DIR/.git-completion.bash" ~
 ln -sfv "$DOTFILES_DIR/.git-prompt.sh" ~
